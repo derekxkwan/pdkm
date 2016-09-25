12c0753cb11a08a923eefbb1a0618f47f5725b1a
derekxkwan <derek.x.kwan@gmail.com>
2016-09-24 21:07:30 -0700

    moving around help files

--- src/dksawlim.c
+++ src/dksawlim.c
@@ -1,6 +1,8 @@
 /* Copyright 2016 - Derek Kwan
  *  * Distributed under GPL v3 */
 
+//new PolyBLEP version!
+
 #include "dkwtab.h"
 
 static t_class *dksawlim_tilde_class;
@@ -8,9 +10,8 @@ static t_class *dksawlim_tilde_class;
 typedef struct _dksawlim_tilde {
 	t_object x_obj;
 	t_float x_freq; //scalar freq
-	double x_phase;
+	double x_phase; //0 <= phs < 1
 	double x_conv; //1/samprate, duration of one sample in seconds
-	double x_saw[TABLEN]; //table for holding saw values
 
 	t_inlet *x_phaselet;
 	t_outlet *x_outlet;
@@ -21,18 +22,7 @@ static void *dksawlim_tilde_new(t_floatarg freq){
 	t_dksawlim_tilde *x = (t_dksawlim_tilde *)pd_new(dksawlim_tilde_class);
 	x->x_freq = freq;
 	x->x_phase = 0.f;
-
-	for(i=0; i<TABLEN; i++){
-		double xval = (double)i/(double)TABLEN;
-		double yval = 0.f;
-		for(j=1; j<=TABDEP; j++){
-			double cterm = 1.f/(double)j;
-			double varterm = sin(xval * (double)j*TPI);
-			double valterm = cterm*varterm;
-			yval += valterm;
-		};
-		x->x_saw[i] = yval;
-	};
+        x->x_conv = 1./(double)sys_getsr(); //amount to change phase for freq 1
 
 	x->x_phaselet = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("ft1"));
 	x->x_outlet = outlet_new(&x->x_obj, gensym("signal"));
@@ -44,30 +34,21 @@ static t_int *dksawlim_tilde_perform(t_int *w){
 	 t_float *in = (t_float *)(w[2]);
 	t_float *out = (t_float *)(w[3]);
 	int n = (int)(w[4]);
-	
-	while(n--){
-		double freq = (double) *in++;
-		x->x_phase = fmod(x->x_phase, (double)(TABLEN));//phases need to be wrapped  just in case
-		while(x->x_phase < 0){
-			x->x_phase += TABLEN;
-		};
-		double dtabphase = x->x_phase;//find corresponding index for table 
-		int tabphase1 = (int)dtabphase;
-		int tabphase2 = tabphase1 + 1;
-		double frac = (double)dtabphase - tabphase1;
-		if(tabphase1 >= (TABLEN - 1)){
-			tabphase1 = TABLEN - 1; //checking to see if index falls within bounds
-			tabphase2 = 0;
-		}
-		else if(tabphase1 < 0){
-			tabphase1 = 0;
-			tabphase2 = 1;
-		};
-		double yb = x->x_saw[tabphase2]; //linear interp
-		double ya = x->x_saw[tabphase1];
-		double output = ya+((yb-ya)*frac);
-		*out++ = output;
-		x->x_phase += freq* x->x_conv;
+        int i;	
+	for(i=0;i<n;i++){
+            double output;
+            double phs = x->x_phase;
+
+            //our actual desired phase change is conv * freq
+            double conv = x->x_conv * (double)in[i];
+
+            //beginning of phase (near 0)
+            if(phs<conv){
+                double scaled = phs/conv; //scales conv between 0 and 1
+                //our interpolating poly is -x^2+2x-1
+                output = 
+
+            };
 	};
 	return(w+5);
 }
@@ -76,13 +57,19 @@ static void dksawlim_tilde_dsp(t_dksawlim_tilde *x, t_signal **sp){
 
 	//(freq*tablelen)/(samplerate) = array values per sample to advance
 	// divide by tablelen to map to 0 to 1 range,..freq/samplerate
-    x->x_conv = (double)TABLEN/(double)sp[0]->s_sr; //amount to change phase for freq 1
+    x->x_conv = 1./(double)sp[0]->s_sr; //amount to change phase for freq 1
 	    dsp_add(dksawlim_tilde_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
 
 }
 
 static void dksawlim_tilde_ft1(t_dksawlim_tilde *x, t_float phase){
-	x->x_phase = (double)TABLEN*phase;
+        if(phase < 0){
+            phase = 0.;
+        }
+        else if(phase > 1.){
+            phase = 1.;
+        };
+	x->x_phase = (double)phase;
 
 }
 
